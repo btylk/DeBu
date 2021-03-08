fce71657768fbc5e8da6ac7bb07e646be0e1b65b
btylk <55312210+btylk@users.noreply.github.com>
2021-03-08 23:20:02 +0700

    add graph

--- screens/ReportScreen.js
+++ screens/ReportScreen.js
@@ -8,7 +8,15 @@ import {
   TouchableHighlight,
   View
 } from "react-native";
-
+import {
+    LineChart,
+    BarChart,
+    PieChart,
+    ProgressChart,
+    ContributionGraph,
+    StackedBarChart
+  } from "react-native-chart-kit";
+import { Dimensions } from "react-native";
 class ReportScreen extends React.Component {
     state = {
     // modalVisible: false
@@ -40,22 +48,118 @@ class ReportScreen extends React.Component {
     render() {
         // const { modalVisible } = this.state;
         // const {data} = this.props.route.params
+        const screenWidth = Dimensions.get("window").width;
         const { navigation } = this.props; 
         const data_report = navigation.getParam('data')
         let JSObj = JSON.parse(data_report)
+        const data = [
+            {
+              name: "% คำเชิงลบต่อตนเอง",
+              population: JSObj.Percent_depress + JSObj.Percent_desolate,
+              color: "#93C2C6",
+              legendFontColor: "#7F7F7F",
+              legendFontSize: 12
+            },
+            {
+              name: "% คำเชิงลบต่อผู้อื่น",
+              population: JSObj.Percent_bully,
+              color: "#FDB196",
+              legendFontColor: "#7F7F7F",
+              legendFontSize: 12
+            },
+            {
+              name: "% คำทั่วไป",
+              population: JSObj.Percent_left,
+              color: "#F7E37B",
+              legendFontColor: "#7F7F7F",
+              legendFontSize: 12
+            },
+        ];
+        const chartConfig = {
+            backgroundGradientFrom: "#1E2923",
+            backgroundGradientFromOpacity: 0,
+            backgroundGradientTo: "#08130D",
+            backgroundGradientToOpacity: 0.5,
+            color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+            strokeWidth: 2, // optional, default 3
+            barPercentage: 0.5,
+            useShadowColorFromDataset: false // optional
+        };
         return (
         <View style={styles.container}>
             <Text style={styles.header}>รายงาน</Text>
-                <View style={styles.centeredView}>
-                <Text>หัวข้อ: {JSON.stringify(JSObj.Title)}</Text>
-                <Text>ขั้วอารมณ์ความคิดเห็น: {JSON.stringify(JSObj.Polarity)}</Text>
-                <Text>ความคิดเห็น: {JSON.stringify(JSObj.Sentiment)}</Text>
-                <Text>ประกาศหรือโฆษณา: {JSON.stringify(JSObj.Announcement)}</Text>
-                <Text>ร้อยละคะแนนความมั่นใจ: {JSON.stringify(JSObj.Score)}</Text>
-                <Text>คำทั้งหมด: {JSON.stringify(JSObj.All_word)}</Text>
-                <Text>คำที่พบ: {JSON.stringify(JSObj.Word_found)}</Text>
-                <Text>ร้อยละของคำที่พบ: {JSON.stringify(JSObj.Percent)}</Text>
+            <Text style={styles.title}>ผลการวิเคราะห์ข้อมูล</Text>
+            <View style={styles.graph}> 
+            <PieChart
+                data={data}
+                width={screenWidth}
+                height={200}
+                chartConfig={chartConfig}
+                accessor={"population"}
+                backgroundColor={"transparent"}
+                paddingLeft={"0"}
+                center={[10, 10]}
+                absolute
+                
+            />
+            {/* <LineChart
+                    data={{
+                    labels: ["January", "February"],
+                    datasets: [
+                        {
+                        data: [
+                            Math.random() * 100,
+                            Math.random() * 100,
+                            Math.random() * 100,
+                            JSObj.Percent_found
+                        ]
+                        }
+                    ]
+                    }}
+                    width={Dimensions.get("window").width-40} // from react-native
+                    height={220}
+                    yAxisLabel=""
+                    yAxisSuffix="%"
+                    yAxisInterval={1} // optional, defaults to 1
+                    chartConfig={{
+                    backgroundColor: "#e26a00",
+                    backgroundGradientFrom: "#fb8c00",
+                    backgroundGradientTo: "#ffa726",
+                    decimalPlaces: 2, // optional, defaults to 2dp
+                    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                    labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                    style: {
+                        borderRadius: 16
+                    },
+                    propsForDots: {
+                        r: "6",
+                        strokeWidth: "2",
+                        stroke: "#ffa726"
+                    }
+                    }}
+                    bezier
+                    style={{
+                    marginVertical: 8,
+                    borderRadius: 16
+                    }}
+                 /> */}
             </View>
+                <View style={styles.centeredView}>
+                    <Text style={styles.line_space}>หัวข้อ: {JSON.stringify(JSObj.Title)}</Text>
+                    <Text style={styles.line_space}>ขั้วอารมณ์ความคิดเห็น: {JSON.stringify(JSObj.Polarity)}</Text>
+                    <Text style={styles.line_space}>ความคิดเห็น: {JSON.stringify(JSObj.Sentiment)}</Text>
+                    {/* <Text style={styles.line_space}>ประกาศหรือโฆษณา: {JSON.stringify(JSObj.Announcement)}</Text>
+                    <Text style={styles.line_space}>ร้อยละคะแนนความมั่นใจ: {JSON.stringify(JSObj.Score)}</Text> */}
+                    <Text style={styles.line_space}>คำทั้งหมด: {JSON.stringify(JSObj.All_word)} คำ</Text>
+                    <Text style={styles.line_space}>คำเชิงลบต่อผู้อื่น</Text>
+                    <Text style={styles.line_space_2nd}>{'\u25cf'} คำที่มีอารมณ์ตำหนิ รังเกียจ: {JSON.stringify(JSObj.Other_person.Bully_found)}</Text>
+                    <Text style={styles.line_space}>คำเชิงลบต่อตนเอง</Text>
+                    <Text style={styles.line_space_2nd}>{'\u25cf'} คำที่มีอารมณ์ไม่สบายใจ กดดัน: {JSON.stringify(JSObj.Itself.Desolate_found)}</Text>
+                    <Text style={styles.line_space_2nd}>{'\u25cf'} คำที่มีอารมณ์เศร้า หดหู่: {JSON.stringify(JSObj.Itself.Depress_found)}</Text>
+                    <Text style={styles.line_space}>เปอร์เซ็นต์ของคำที่พบ: {JSON.stringify(JSObj.Percent_found)} %</Text>
+                    <Text style={styles.line_space}>เปอร์เซ็นต์ของคำที่เหลือ: {JSON.stringify(JSObj.Percent_left)} %</Text>
+                    {/* <Text>จำนวนคำซึมเศร้าที่พบ: {JSON.stringify(JSObj.Other_person.All_depress)}</Text> */}
+                </View>
         </View>
         );
     }
@@ -76,33 +180,40 @@ const styles = StyleSheet.create({
     centeredView: {
         // flex: 1,
         justifyContent: "center",
-        alignItems: "center",
-        marginTop: 80,
+        textAlign: "left",
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 12,
+        marginTop: 10,
         marginLeft: 20,
         marginRight: 20,
         borderRadius: 6,
-        backgroundColor: "#E9446A",
-        color: "#FFFFFF"
-
-
-        
+        color: "#FFFFFF",
+        backgroundColor: "#FA8072", 
     },
-    modalView: {
-        margin: 20,
-        backgroundColor: "white",
-        borderRadius: 20,
-        padding: 120,
-        alignItems: "center",
-        shadowColor: "#000",
-        shadowOffset: {
-        width: 0,
-        height: 2
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
+    line_space:{
+        borderBottomWidth: 5,
+        borderBottomColor: "#FA8072"
+        // color: "#7F7F7F"
+    },
+    line_space_2nd:{
+        borderBottomWidth: 5,
+        borderBottomColor: "#FA8072",
+        marginLeft: 15
+        // color: "#7F7F7F"
+    },
+    graph:{
         justifyContent: "center",
-        alignContent: "center",
+        alignItems: "center",
+        // backgroundColor: '#FAD2A7',
+        marginTop: 10,
+        borderRadius: 10
+
+    },
+    title:{
+        marginTop: 20,
+        marginLeft: 20,
+        color: "#7F7F7F"
     },
     openButton: {
         backgroundColor: "#F194FF",
